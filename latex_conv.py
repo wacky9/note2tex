6ab3d6eac73ca2629e74464bf0384dd835b3e358
hredder <hredder3@gmail.com>
2024-02-17 11:48:21 -0500

    changes

--- latex_conv.py
+++ latex_conv.py
@@ -1,7 +1,29 @@
 
-def parseToken(token):
+tokenMapping = {
+  "\plus": "+",
+  "\minus": "\\",
+  "\\times": "\\cdot",
+  "\divide": "/",
+  "!": "!",
+  "\comma": ",",
+
+  "(": "(",
+  ")": ")",
+
+  "\\startsqrt": "\sqrt{",
+  "\\endqrt": "}",
+
+  "\startexp": "^{",
+  "\endexp": "}"
+
+}
+
+def writeToken(latexFile, token):
     pass
 
+def writeNewLine(latexFile):
+    latexFile.write("\\\\")
+
 if __name__ == '__main__':
-    latexFile = open("latex.tex", w)
-    print()
+    latexFile = open("latex.tex", 'w')
+    writeNewLine(latexFile)
