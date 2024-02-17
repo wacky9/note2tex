1ac601381a33b17d514aaa303473b08b51fb5954
tobiasSimpson <93829921+tobiasSimpson@users.noreply.github.com>
2024-02-17 11:49:45 -0500

    latex header and footer

--- latex_conv.py
+++ latex_conv.py
@@ -1,7 +1,29 @@
 
+
+def writeFileHeader(latexFile, titleName, authorName):
+    print("\\documentclass{article}", file=latexFile)
+    print("\\usepackage[utf8]{inputenc}", file=latexFile)
+    print("\\usepackage[english]{babel}", file=latexFile)
+    print("\\usepackage{enumitem}", file=latexFile)
+    print("\\usepackage{amsmath}", file=latexFile)
+    print("\\usepackage{graphicx}", file=latexFile)
+    print("\\usepackage[]{amsthm}", file=latexFile)
+    print("\\usepackage[]{amssymb}", file=latexFile)
+    print("\\theoremstyle{remark}", file=latexFile)
+    print("\\title{" + titleName + "}", file=latexFile)
+    print("\\author{" + authorName + "}", file=latexFile)
+    print("\\date\\today", file=latexFile)
+    print("\\begin{document}", file=latexFile)
+    print("\\maketitle", file=latexFile)
+
+def writeFileFooter(latexFile):
+    print("\\end{document}", file=latexFile)
+
 def parseToken(token):
     pass
 
 if __name__ == '__main__':
-    latexFile = open("latex.tex", w)
-    print()
+    latexFile = open("latex.tex", "w")
+    writeFileHeader(latexFile, "Test", "Toby")
+    writeFileFooter(latexFile)
+    
